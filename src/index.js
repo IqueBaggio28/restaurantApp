58cc064328ee65b7fcbee1d8b7b9144254ca1f38
IqueBaggio28 <134702403+IqueBaggio28@users.noreply.github.com>
2024-06-17 19:51:05 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import App_2 from './App_2.js'
+// import App_2 from './App_2.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App_2 />
+    <App/>
   </React.StrictMode>
 );
 
